1d76b9bb1208cfb766cc2672ece59e027dd46118
diskonnekted <arif.susilo@gmail.com>
2026-06-05 08:54:34 +0700

    Add global validation error display to report form

--- resources/views/livewire/report-form.blade.php
+++ resources/views/livewire/report-form.blade.php
@@ -51,6 +51,28 @@
             </div>
         </div>
 
+        @if ($errors->any())
+            <div class="mb-6 p-4 bg-red-50 border-l-4 border-red-500 rounded-r-lg">
+                <div class="flex">
+                    <div class="flex-shrink-0">
+                        <svg class="h-5 w-5 text-red-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                            <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.78 7.72a.75.75 0 00-1.06 1.06L8.94 10l-1.22 1.22a.75.75 0 101.06 1.06L10 11.06l1.22 1.22a.75.75 0 101.06-1.06L11.06 10l1.22-1.22a.75.75 0 00-1.06-1.06L10 8.94 8.78 7.72z" clip-rule="evenodd" />
+                        </svg>
+                    </div>
+                    <div class="ml-3">
+                        <h3 class="text-sm font-medium text-red-800">Terdapat kesalahan pada isian Anda:</h3>
+                        <div class="mt-2 text-sm text-red-700">
+                            <ul role="list" class="list-disc space-y-1 pl-5">
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        @endif
+
         <div class="bg-white rounded-2xl shadow-xl border border-gray-100 overflow-hidden">
             <form wire:submit.prevent="submit" class="p-8">
                 
